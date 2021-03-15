4142635f3f32ac912a94fe66e0362e9723296645
Stenio L. de Souza <steniols@gmail.com>
2021-03-15 13:31:44 -0300

    Adicionando o componente Link

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,14 @@
+import Link from 'next/link';
+
 function Home() {
-    return <div>Página inicial</div>
+    return (
+        <div>
+            <h1>Página inicial</h1>
+            <Link href="/sobre">
+                <a>Acessar página sobre</a>
+            </Link>
+        </div>
+    )
 }
 
 export default Home

939644406a0cdbc0b3df3e43f3f59bc181c11627
Stenio L. de Souza <steniols@gmail.com>
2021-03-15 15:42:47 -0300

    API Fixes

--- pages/api/tempo.js
+++ pages/api/tempo.js
@@ -1,8 +1,8 @@
-function tempo(request, response) {
+async function tempo(request, response) {
     const dynamicDate = new Date(); 
 
-    const subscribersResponse = await fetch("https://api.convertkit.com/v3/subscribers?api_secret=Mt7SNE81zlB_ZVk-H9n5nXwakV80TEO5yNc4Fxhd11Y&from=2016-02-01&to=2021-03-15");
-    const subscribersResponseJson = await subscribersResponse.json();
+    const subscribersResponse = await fetch("https://api.convertkit.com/v3/subscribers?api_secret=Mt7SNE81zlB_ZVk-H9n5nXwakV80TEO5yNc4Fxhd11Y");
+    const subscribersResponseJson = await subscribersResponse.json();  
     const inscritos = subscribersResponseJson.total_subscribers;
 
     response.json({
